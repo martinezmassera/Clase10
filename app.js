38bb96c65d0e06a12b825fc53fa394455a86b07e
unknown <martinezmassera@gmail.com>
2022-05-29 19:55:56 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,67 @@
+const { Router } = require('express')
+const express = require('express')
+const multer = require('multer')
+const Contenedor = require('./contenedor')
+const prod = new Contenedor('./productos.json')
+
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'))
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'public/file/')
+    },
+    filename: (req, file, cb) => {
+        const t = Date.now()
+        cb(null, t + file.originalname)
+
+    }
+})
+
+const upload = multer({ storage })
+
+app.post('/upload', upload.single('file'),(req, res) => {
+    const name = req.body.name;
+    const price = req.body.price;
+    const thumbnail = req.body.thumbnail;
+    const img = req.file.filename;
+    console.log(img)
+    prod.addItemForm(name, price, img)
+    res.send(`<h1>${name}</h1><br><h1>$ ${price}</h1><br><h1>${thumbnail}</h1><br><img src=/file/${img} />`)
+})
+
+
+const router = Router()
+router.get('/', async (req, res) => {
+    res.send(await prod.leer())
+})
+
+router.get('/:id', async (req, res) => {
+    const id = req.params.id
+    res.send(await prod.getId(id))
+})
+
+router.post('/', (req, res) => {
+    const body = req.body
+    prod.addItem(body)
+    res.send('producto agregado')
+})
+
+router.put('/:id', async (req, res) => {
+    await prod.editById(req.params.id, req.body)
+    res.send('producto actualizado')
+})
+
+router.delete('/:id', async (req, res) => {
+    await prod.deleteById(req.params.id)
+    res.send('producto eliminado')
+})
+
+app.use('/api/productos', router)
+const PORT = 8080
+const server = app.listen(PORT, () => {
+    console.log(`Servidor http en el puerto ${PORT}`)
+})
+server.on("error", error => console.log(`Error en el servidor! - ${error}`))
---
+++ contenedor.js
@@ -0,0 +1,94 @@
+const fs = require('fs')
+let countId = 1
+class Contenedor {
+    constructor(archivo) {
+        this.archivo = archivo
+        this.productos = []
+    }
+
+    async leer() {
+        try {
+            const datos = await fs.promises.readFile(this.archivo)
+            const datosParse = JSON.parse(datos)
+            this.productos = datosParse
+            return this.productos
+        } catch (error) {
+            return { error: 'archivo no encontrado' }
+        }
+    }
+
+    async write() {
+        const stringProd = JSON.stringify(this.productos)
+        await fs.promises.writeFile(this.archivo, stringProd)
+    }
+
+    async getId(id) {
+        try {
+            const leer = await this.leer()
+            const item = leer.find(i => i.id == id)
+            console.log(item)
+            if (!item) {
+                return { error: 'producto no encontrado' }
+            } else {
+                return item
+            }
+        } catch (error) {
+            return { error: 'producto no encontrado' }
+        }
+    }
+
+    countId() {
+
+        const id = countId++;
+        return id
+    }
+
+    async addItem(body) {
+        const id = this.countId()
+
+        body['id'] = id
+        this.productos.push(body)
+        console.log(body)
+        await this.write()
+    }
+
+    async addItemForm(title, price, thumbnail) {
+        const id = this.countId()
+        this.productos.push({ "title": title, "price": price, "thumbnail": thumbnail,"id": id })
+        console.log(this.productos)
+        //await this.write()
+
+    }
+
+
+
+
+    async deleteById(number) {
+        try {
+            const index = this.productos.findIndex((item) => item.id == number)
+            if (index != -1) {
+                this.productos.splice(index, 1)
+            }
+            console.log(this.productos)
+            //await this.write()
+        }
+        catch (error) {
+            return { error: 'producto no encontrado' }
+        }
+    }
+
+    async editById(number, body) {
+        const aEditar = [...this.productos]
+        try {
+            const index = this.productos.findIndex((item) => item.id == number)
+            aEditar[index] = { ...aEditar[index], ...body }
+            this.productos = aEditar
+            console.log(this.productos)
+            //await this.write()
+        } catch (error) {
+            return { error: 'producto no encontrado' }
+        }
+    }
+}
+
+module.exports = Contenedor
