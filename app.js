45116994e6b7ab773fdb9502a29dc88b0964f60e
unknown <martinezmassera@gmail.com>
2022-05-30 00:07:16 -0300

    first commit

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(express.static('public'))
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, 'public/file/')
+        cb(null, './public/file/')
     },
     filename: (req, file, cb) => {
         
