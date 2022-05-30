d3be603cf84cd55f2950aeefe9914c2dcf3826cb
unknown <martinezmassera@gmail.com>
2022-05-30 00:55:56 -0300

    first commit2

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 8080
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use(express.static('/public'))
+app.use(express.static('public'))
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
